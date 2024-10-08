5c7b34dfc8d31aca10143f5c7b74ead7fcc157a4
Thibault <tcettier@asi.fr>
2024-10-08 15:39:54 +0200

    Improve header and boat page

--- web/pirate-s-odyssey/src/app/components/boat/boat.component.ts
+++ web/pirate-s-odyssey/src/app/components/boat/boat.component.ts
@@ -1,6 +1,19 @@
-import { Component, inject, OnInit, signal, Signal, WritableSignal } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component, Signal, signal } from '@angular/core';
+import { Resources } from '../../models/resources.model';
+
+interface CrewMember {
+  name: string;
+  role: string;
+  health: number;
+}
+
+interface Upgrade {
+  name: string;
+  description: string;
+  cost: number;
+  resourceType: string;
+}
 
 @Component({
   selector: 'app-boat',
@@ -9,13 +22,22 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./boat.component.scss'],
   imports: [CommonModule]
 })
-export class BoatComponent implements OnInit {
+export class BoatComponent {
+  boatHealth: Signal<number> = signal(25);
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) { }
+  crewMembers: Signal<CrewMember[]> = signal([
+    { name: 'Jack Sparrow', role: 'Captain', health: 100 },
+    { name: 'Anne Bonny', role: 'First Mate', health: 90 },
+    { name: 'Blackbeard', role: 'Gunner', health: 85 }
+  ]);
 
-  ngOnInit(): void {
+  upgrades: Upgrade[] = [
+    { name: 'Improved Sails', description: 'Increase speed', cost: 50, resourceType: 'wood' },
+    { name: 'Reinforced Hull', description: 'Increase durability', cost: 100, resourceType: 'wood' },
+    { name: 'Extra Cannons', description: 'Increase firepower', cost: 150, resourceType: 'iron' }
+  ];
+
+  applyUpgrade(upgrade: Upgrade) {
+    console.log(`Applying upgrade: ${upgrade.name}`);
   }
 }
--- web/pirate-s-odyssey/src/app/components/shared/header/header.component.ts
+++ web/pirate-s-odyssey/src/app/components/shared/header/header.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
+import { Resources } from '../../../models/resources.model';
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,5 +12,15 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
+
 export class HeaderComponent {
+
+   resources : Resources[] = [
+    { resourceType: 'Wood', amount: 150, img:'wood' },
+    { resourceType: 'Gold', amount: 50, img:'gold' },
+    { resourceType: 'Food', amount: 75, img:'food' },
+    { resourceType: 'Iron', amount: 10, img:'iron' },
+    { resourceType: 'Rhum', amount: 110, img:'rhum' }
+  ];
 }
