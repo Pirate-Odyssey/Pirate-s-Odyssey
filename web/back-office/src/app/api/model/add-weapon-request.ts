3e8a930e2d09122ff6ce08211f37bcb1a881ec4e
Paul Poupet <ppoupet@asi.fr>
2024-10-18 07:43:42 +0200

    update request

--- web/back-office/src/app/api/model/add-weapon-request.ts
+++ web/back-office/src/app/api/model/add-weapon-request.ts
@@ -13,7 +13,7 @@ import { ItemRarity } from './item-rarity';
 
 export interface AddWeaponRequest { 
     damage?: number;
-    speed?: string;
+    speed?: number;
     twoHanded?: boolean;
     name?: string | null;
     description?: string | null;
