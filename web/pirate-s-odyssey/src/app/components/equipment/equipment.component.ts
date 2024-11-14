b6dd18099e110b33b1ab38f06d423d2e39b8b7b0
Thibault <tcettier@asi.fr>
2024-11-14 14:39:43 +0100

    Dynamic data in equipment and shop section

--- web/pirate-s-odyssey/src/app/components/equipment/equipment.component.ts
+++ web/pirate-s-odyssey/src/app/components/equipment/equipment.component.ts
@@ -1,21 +1,54 @@
-import { Component, inject, OnInit, signal, Signal, WritableSignal } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { ItemContainerComponent, SelectorComponent } from '@po/common';
+import { EquipmentResponse, EquipmentService } from '../../api';
 
 @Component({
-  selector: 'app-equipment',
+  selector: 'po-equipment',
   templateUrl: './equipment.component.html',
   standalone: true,
   styleUrls: ['./equipment.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, ItemContainerComponent, SelectorComponent]
 })
 export class EquipmentComponent implements OnInit {
+  private readonly equipmentService = inject(EquipmentService);
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) { }
+  // Les options disponibles dans le shop (basées sur les types d'équipements)
+  shopOptions = ['Helmet', 'Sword', 'Boots', 'Shield'];
+
+  // Option sélectionnée par défaut
+  selectedOption = signal(this.shopOptions[0]);
+
+  // Les items à afficher dans les lib-item-container
+  shopItems = signal<EquipmentResponse[]>([]);
+
+  // Propriété pour stocker les items filtrés
+  filteredItems = computed(() =>
+    this.shopItems().filter(
+      (item) => item.equipmentType === this.selectedOption()
+    )
+  );
+
+  // filteredItems = computed(() => {
+  //   const items = this.shopItems().filter(
+  //     (item) => item.equipmentType === this.selectedOption()
+  //   );
+  //   console.log(items); // Ajoutez ceci pour vérifier le contenu des items
+  //   return items;
+  // });
 
   ngOnInit(): void {
+    // Appel de l'API pour obtenir les équipements
+    this.equipmentService.getEquipments().subscribe({
+      next: (response: EquipmentResponse[]) => {
+        console.log('response', response);
+        this.shopItems.set(response); // Utilisation directe de la réponse typée
+      }
+    });
+  }
+
+  // Méthode appelée lors du changement de sélection
+  onSelectionChange(option: string) {
+    this.selectedOption.set(option); // Mise à jour de la sélection avec le signal
   }
 }
--- web/pirate-s-odyssey/src/app/components/shop/shop.component.ts
+++ web/pirate-s-odyssey/src/app/components/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { ItemContainerComponent, SelectorComponent } from '@po/common';
+import { EquipmentResponse, EquipmentService } from '../../api';
 
 @Component({
   selector: 'po-shop',
@@ -9,81 +10,42 @@ import { ItemContainerComponent, SelectorComponent } from '@po/common';
   styleUrls: ['./shop.component.scss'],
   imports: [CommonModule, ItemContainerComponent, SelectorComponent]
 })
-export class ShopComponent {
-  // Les options disponibles dans le shop
-  shopOptions = ['Sword', 'Boots', 'Shield'];
+export class ShopComponent implements OnInit {
+  private readonly equipmentService = inject(EquipmentService);
+
+  // Les options disponibles dans le shop (basées sur les types d'équipements)
+  shopOptions = ['Helmet', 'Sword', 'Boots', 'Shield', 'Chestplate'];
 
   // Option sélectionnée par défaut
   selectedOption = signal(this.shopOptions[0]);
 
   // Les items à afficher dans les lib-item-container
-  shopItems = signal([
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Sword',
-      itemDescription: 'A sharp blade for close combat.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Boots',
-      itemDescription: 'Fast and sturdy boots for quick movements.',
-      buttonText: 'Buy'
-    },
-    {
-      itemName: 'Shield',
-      itemDescription: 'A strong shield to block incoming attacks.',
-      buttonText: 'Buy'
-    }
-  ]);
+  shopItems = signal<EquipmentResponse[]>([]);
 
   // Propriété pour stocker les items filtrés
   filteredItems = computed(() =>
-    this.shopItems().filter((item) => item.itemName === this.selectedOption())
+    this.shopItems().filter(
+      (item) => item.equipmentType === this.selectedOption()
+    )
   );
+
+  // filteredItems = computed(() => {
+  //   const items = this.shopItems().filter(
+  //     (item) => item.equipmentType === this.selectedOption()
+  //   );
+  //   console.log(items); // Ajoutez ceci pour vérifier le contenu des items
+  //   return items;
+  // });
+
+  ngOnInit(): void {
+    // Appel de l'API pour obtenir les équipements
+    this.equipmentService.getEquipments().subscribe({
+      next: (response: EquipmentResponse[]) => {
+        console.log('response', response);
+        this.shopItems.set(response); // Utilisation directe de la réponse typée
+      }
+    });
+  }
 
   // Méthode appelée lors du changement de sélection
   onSelectionChange(option: string) {
